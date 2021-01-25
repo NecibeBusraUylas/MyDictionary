ef0a2069f4769e1453cb9319312b165387739b67
Büşra Uylaş <busrauylas01@gmail.com>
2021-01-25 12:21:32 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<KeyT,ValueT>
+    {
+        KeyT[] keyArray;
+        KeyT[] tempKeyArray;
+        ValueT[] valueArray;
+        ValueT[] tempValueArray;
+
+        public MyDictionary()
+        {
+            keyArray = new KeyT[0];
+            valueArray = new ValueT[0];
+        }
+
+        public void Add(KeyT key,ValueT value)
+        {
+            if(Array.IndexOf(keyArray,key) == -1)
+            {
+                tempKeyArray = keyArray;
+                keyArray = new KeyT[keyArray.Length + 1];
+                for (int i = 0; i < tempKeyArray.Length; i++)
+                {
+                    keyArray[i] = tempKeyArray[i];
+                }
+                keyArray[keyArray.Length - 1] = key;
+
+                tempValueArray = valueArray;
+                valueArray = new ValueT[valueArray.Length + 1];
+                for (int i = 0; i < tempValueArray.Length; i++)
+                {
+                    valueArray[i] = tempValueArray[i];
+                }
+                valueArray[valueArray.Length - 1] = value;
+            }
+
+            else
+            {
+                Console.WriteLine("\nKey must be unique!" + " "+ "Key index " + key + " is not empty. Enter another key for" + " " + value + ".");
+                return;
+            }
+        }
+
+        public void ListTheDictionary()
+        {
+            for (int i=0 ; i < keyArray.Length ; i++)
+            {
+                Console.WriteLine("( " + keyArray[i] + " , " + valueArray[i]  + " )");
+            }
+        }       
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "C#");
+            myDictionary.Add(2, "Python");
+            myDictionary.Add(3, "C");
+            myDictionary.Add(4, "Java");
+            myDictionary.Add(5, "C++");
+
+            Console.WriteLine("\n -----My Dictionary-----");
+            myDictionary.ListTheDictionary();
+
+            myDictionary.Add(2, "PHP");
+            myDictionary.Add(6, "HTML");
+
+            Console.WriteLine("\n -----My Dictionary-----");
+            myDictionary.ListTheDictionary();
+        }
+    }
+}
